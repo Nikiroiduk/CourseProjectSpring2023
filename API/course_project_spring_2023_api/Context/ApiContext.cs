b9fa5b12f37c39f07188131127d0d5accf3c873b
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2023-04-07 19:08:25 +0300

    a little cleaner (1935)

--- API/course_project_spring_2023_api/Context/ApiContext.cs
+++ API/course_project_spring_2023_api/Context/ApiContext.cs
@@ -9,45 +9,17 @@ namespace course_project_spring_2023_api.Context
         {
         }
 
-        public DbSet<Person> Persons { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Exercise> Exercises { get; set; }
-        public DbSet<Repetition> Repetitions { get; set; }
-        public DbSet<Post> Posts { get; set; }
-        public DbSet<TrainingCourse> TrainingCourses { get; set; }
-        public DbSet<UserCourse> UserCourses { get; set; }
+        //public DbSet<Person> Persons { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<Exercise> Exercises { get; set; }
+        //public DbSet<Repetition> Repetitions { get; set; }
+        //public DbSet<Post> Posts { get; set; }
+        //public DbSet<TrainingCourse> TrainingCourses { get; set; }
+        //public DbSet<UserCourse> UserCourses { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>()
-                .HasMany(e => e.Courses)
-                .WithOne(e => e.User)
-                .HasForeignKey(e => e.UserId)
-                .IsRequired();
-
-            modelBuilder.Entity<UserCourse>()
-                .HasOne(e => e.User)
-                .WithMany(e => e.Courses)
-                .HasForeignKey(e => e.UserId)
-                .IsRequired();
-
-            //modelBuilder.Entity<TrainingCourse>()
-            //    .HasMany(e => e.Exercises);
-
-            modelBuilder.Entity<Exercise>()
-                .HasMany(e => e.Repetitions)
-                .WithOne(e => e.Exercise)
-                .HasForeignKey(e => e.ExerciseId)
-                .IsRequired();
-
-            modelBuilder.Entity<Repetition>()
-                .HasOne(e => e.Exercise)
-                .WithMany(e => e.Repetitions)
-                .HasForeignKey(e => e.ExerciseId)
-                .IsRequired();
-
-            //modelBuilder.Entity<Post>()
-            //    .HasOne(e => e.TrainingCourse);
+            
         }
     }
 }
